f4c13a1d567514c7ffd39f5b7f739683ab5f4b2e
Ebisa-Tefera <ebisatefera203@gmail.com>
2022-06-10 01:41:53 -0700

    0th task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,44 @@
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+root@38a1d980bbef:/alx-low_level_programming/0x04-more_functions_nested_loops# cat> 0-isupper.c
+#include "main.h"
+/**
+  * _isupper - check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
+{
+        if (x >= 65 && x <= 90)
+        {
+                return (1);
+        }
+        
+        return (0)
+}
+root@38a1d980bbef:/alx-low_level_programming/0x04-more_functions_nested_loops# git add .
+* Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
+{
+        if (x >= 65 && x <= 90)
+        {
+                return (1);
+        }
+
+        return (0)
+root@38a1d980bbef:/alx-low_level_programming/0x04-more_functions_nested_loops# git add .
+root@38a1d980bbef:/alx-low_level_programming/0x04-more_functions_nested_loops# git commit -m "0th task"
+root@38a1d980bbef:/alx-low_level_programming/0x04-more_functions_nested_loops# git push
+clear
